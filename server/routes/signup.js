6345ca3b8c10d7b64f6005d2ff417429fbc90f79
Boris N'KUAKO <jobs.bobo@yahoo.com>
2021-11-29 20:15:27 +0100

    added: signup working + modified jwtgenerator file

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
 const pool = require('../db')
+const jwtGenerator = require('../jwtGenerator')
 
-
-router.post('/signup', async (req,res)=>{
+router.post('/', async (req,res)=>{
   
   // 1. destructure req.body
-  const {email, password, first_name, last_name} = req.body
+  const {email, password, first_name, last_name, created_at} = req.body
   
   try {
     // 2. add bcrypt to password
@@ -14,12 +14,24 @@ router.post('/signup', async (req,res)=>{
     const salt = await bcrypt.genSalt(saltRound)
     const bcryptPassword = await bcrypt.hash(password,salt)
 
-    // 3. add req.body to db
+    // 3. check if user exist in db
+    const checkUser = await pool.query('SELECT * FROM users WHERE email=($1)',[email])
     
-    const newUser = pool.query('INSERT INTO users (email, password, first_name, last_name) VALUES ($1,$2,$3)'
-    [email,bcryptPassword, first_name, last_name])
-    
-    //4. generate token 
+    if(checkUser.rows[0] === undefined){
+
+      // 4. add req.body to db
+      const newUser = await pool.query('INSERT INTO users (email, password, first_name, last_name, created_at) VALUES($1,$2,$3,$4,$5) RETURNING *',
+      [email,bcryptPassword,first_name,last_name,created_at])
+       
+      //5. generate token
+       const token = jwtGenerator(newUser.rows[0].id)
+
+       //6. send token to res
+       res.json({token})
+
+    } else{
+      res.status(401).json('un utilisateur existe dejà avec cet email..')
+    }
 
   } catch (error) {
     console.error('⛔ error ⛔: '+ error.message);
