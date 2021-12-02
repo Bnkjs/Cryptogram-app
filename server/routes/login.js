c9ccd54c251099794fd6a6003ed95d11e3d9fefa
Boris N'KUAKO <jobs.bobo@yahoo.com>
2021-12-02 16:22:26 +0100

    modified: refacto + added delete req to contact

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,28 +6,20 @@ const authorization = require('../middleware/authorization')
 
 
 router.post('/', async (req,res)=>{
-  
-  // 1. destructure req.body
   const {email,password} = req.body
-  
-  try {
-    
-    // 2. find user where email match user db
-    const checkUserExist = await pool.query('SELECT * FROM users WHERE email=($1)',
-    [email])
+  const checkUserExist = await pool.query('SELECT * FROM users WHERE email=($1)',[email])
 
+
+  try {
     if(checkUserExist.rows[0] === undefined){
       res.status(401).json('l\'email ou le mot de passe est incorrect')
     }else{
-      //3. compare password db
       const validPassport = await bcrypt.compare(password, checkUserExist.rows[0].password)
-      
       if(!validPassport){
         res.status(401).json('l\'email ou le mot de passe est incorrect');
-      }else{
-          // 4. generate token 
-          const token = jwtGenerator(checkUserExist.rows[0].user_id)
-          res.json({token})
+      }else{   
+        const token = jwtGenerator(checkUserExist.rows[0].user_id)
+        res.json({token})
       }
     }
     
--- server/routes/transfert_crypto.js
+++ server/routes/transfert_crypto.js
@@ -3,16 +3,16 @@ const pool = require('../db')
 const authorization = require('../middleware/authorization')
 const moment = require('moment')
 
-router.post('/', authorization, async (req,res)=>{
-  const findUser = await pool.query('SELECT * FROM users WHERE user_id = ($1)',[req.user])
-  const date = moment().format('DD MMM YYYY H:mm')
-  const generateTrackingId = Math.random().toString(36).substr(2, 40)
+const date = moment().format('DD MMM YYYY H:mm')
+const generateTrackingId = Math.random().toString(36).substr(2, 40)
 
+router.post('/', authorization, async (req,res)=>{
+  const checkUserExist = await pool.query('SELECT * FROM users WHERE user_id = ($1)',[req.user])
   const { crypto_name, amount,description ,card_name } = req.body
+  
   try {
 
-
-    if(findUser.rows[0] === undefined){
+    if(checkUserExist.rows[0] === undefined){
       res.status(404).json('cet utilisateur n\'existe pas!')
   } else{
     const newtransfert = await pool.query('INSERT INTO user_transfert (user_id,card_name,tracking_id,created_at) VALUES ($1,$2,$3,$4) RETURNING *',
