945be9a24305ff2bd69be312c954e5cac1090953
Boris N'KUAKO <jobs.bobo@yahoo.com>
2021-12-13 20:31:23 +0100

    added: order datas to view

---
+++ server/service/apiUrls.js
@@ -0,0 +1,6 @@
+const cryptoApiUrls = {
+  allCoins: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur',
+  coinsById: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids='
+}
+
+module.exports = cryptoApiUrls
