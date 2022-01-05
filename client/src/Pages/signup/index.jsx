eb319f989aa77bcbb0e08d53d5073beb797370fb
Boris N'KUAKO <jobs.bobo@yahoo.com>
2022-01-05 20:30:27 +0100

    modified: signup page

--- client/src/Pages/signup/index.jsx
+++ client/src/Pages/signup/index.jsx
@@ -3,14 +3,17 @@ import { Input } from "../../components/Input/index";
 import { register } from "../../Actions/auth";
 import { useSelector } from "react-redux";
 import { Button } from "../../components/Button/index";
-import { Card } from "../../components/Card";
-import Div from "../../components/Div";
 import { Marged } from "../../components/Marged";
 import { PageContainer } from "../../components/PageContainer";
 import { Form } from "../../components/Form";
-import img_gradient_signup from '../../assets/FORM_SIGNUP_CARD_GRADIENT.svg'
 import { Link } from "react-router-dom";
 import { motion } from 'framer-motion';
+import cubes from 'assets/cube_gradient_blue.svg'
+import './style.scss';
+import { HiOutlineMail } from "react-icons/hi";
+import { BiLockAlt } from "react-icons/bi";
+import { FiFeather } from "react-icons/fi";
+
 const Signup = ({ setAuth }) =>{
   const userLogged = useSelector(state => state.authReducer.isLoggedIn)
   const [inputs, setInputs] = useState({
@@ -41,32 +44,41 @@ const Signup = ({ setAuth }) =>{
        initial={"visible"}
        transition={{ duration: 3 }}          
     >
-      <PageContainer>
-        <Marged />
-        <Div gg="40px" display="grid" gtc="repeat(auto-fill, 550px)" justifyContent='center'  id="class">
-          <Card className="card-form">
-            <img width="450px" height="auto" src={img_gradient_signup} alt="" />
-            <div className="h-card">
-            <span className="h-card-title">S'inscrire</span>
-            <p className="h-card-text">C'est simple et rapide.</p>
-          </div>
-          </Card>
-          <Div  width="550px" display="flex" justifyContent='center' alignItems='start' flexDirection='column'>
+      <PageContainer id="signup-container">
+        <div className="box-form-auth">
+        <div className="header-form-auth">
+         <img className="cubes_form" src={cubes} alt="deux cube avec un dégradé bleu transparent" />
+          <div className="text-header-form-auth">
             <h1 className="title-form">Créez votre compte</h1>
-            <Form onSubmit={(e)=> handleSubmit(e,email,password,username)}>
+            <h2>Commencez <br/> dès maintenant.</h2>
+            <p>En profitant des meilleurs crypto-monnaies du marché.</p>
+          </div>
+        </div>
+        <Form
+         className="form-signup"
+         onSubmit={(e)=> handleSubmit(e,email,password,username)}
+        >    
+            <div className="input-form-icn">            
               <Input role={'textbox'} type="text" placeholder="email" name="email" className="email-input" value={email} onChange={(e)=>onChange(e)} required />
-              <Marged bottom="20px"/>
-              <Input role={'textbox'} type="password" placeholder="mot de passe" name="password" className="mdp-input" value={password} password={password} onChange={(e)=>onChange(e)} required  />
-              <Marged bottom="20px" />
+              <HiOutlineMail className="form-icn"/>
+            </div>
+            <Marged bottom="20px"/>
+            <div className="input-form-icn">
+             <Input role={'textbox'} type="password" placeholder="mot de passe" name="password" className="mdp-input" value={password} password={password} onChange={(e)=>onChange(e)} required  />
+             <BiLockAlt className="form-icn"/>
+            </div>
+            <Marged bottom="20px" />
+            <div className="input-form-icn">
               <Input role={'textbox'} type="text" placeholder="nom d'utilisateur" name="username" className="username-input" value={username} onChange={(e)=>onChange(e)} required />
-              <Marged bottom="20px"/>
-              <Button primary_xl>Créer son compte</Button>
-              <p className="n-login-signup">Vous n’avez pas de compte?
-                <Link to='/login'><span className="link-redirect"> Connectez-vous</span></Link>
-              </p>
-            </Form>
-          </Div>  
-        </Div>
+              <FiFeather className="form-icn"/>
+            </div>
+            <Marged bottom="20px"/>
+            <Button primary_xl>Créer son compte</Button>
+            <p className="n-login-signup">Vous n’avez pas encore de compte?
+              <Link to='/login'><span className="link-redirect"> Connectez-vous</span></Link>
+            </p>
+          </Form>
+        </div>                
       </PageContainer>
     </motion.div>
     
--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-  margin: 0;
+  /* margin: 0;
   padding: 0;
   width: auto;
-  height: auto;
+  height: auto; */
 `
